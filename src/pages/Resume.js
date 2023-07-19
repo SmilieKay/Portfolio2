72acc8c2c0ba6bdc44e9618e20f3c5f7ab6e6337
Cheryl <smilie_kay@yahoo.com>
2023-07-19 18:49:14 -0500

    added resume page info

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -6,7 +6,7 @@ class Resume extends React.Component {
         return (
             <div className={styles.resume}>
                 <h1>My Resume</h1>
-                <p>Thank you for taking the time to learn more about me. Please click the button below to download my resume.</p>
+                <p>Thank you for taking the time to learn more about me. I am proficient in HTML, CSS, JavaScript, Bootstrap, The Dom, APIs, JQuery, JSON, AJAX, Node, ES6, Object-oriented-programming, Express, MySQL, MVC paradigm, Sequelize, Testing, Agile development, Progressive Web Apps, React, NoSQL, MERN Stack, and Computer science fundamentals. Please click the button below to download my resume.</p>
                 <a href="/Resume.pdf" download className={styles.downloadButton}>Download Resume</a>
             </div>
         );
